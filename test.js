eb24cc8ef259a56afd9bfd0c95514e893939bd54
Jodie Zhang <yangzhang@ptc.com>
2022-11-29 17:11:28 +0800

    reword

--- test.js
+++ test.js
@@ -1,5 +1,6 @@
 function test(){
     console.log("test");
-    console.log("second commit")
+    console.log("second commit");
     console.log("third commit");
+    console.log("wrong commit");
 }
